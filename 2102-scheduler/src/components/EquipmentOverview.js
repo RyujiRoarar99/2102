4556275cfb046e900794a106a63f40bcd517527f
pandad0ge <zemuskoh99@gmail.com>
2022-11-03 10:52:12 +0800

    Update EquipmentOverview.js

--- 2102-scheduler/src/components/EquipmentOverview.js
+++ 2102-scheduler/src/components/EquipmentOverview.js
@@ -4,17 +4,18 @@ import Container from "react-bootstrap/Container";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const EquipmentOverview = () => {
     return (
         <Container>
-            <Breadcrumb>
+            <Breadcrumb className="mt-5">
                 <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
                 <Breadcrumb.Item active>Equipment Overview</Breadcrumb.Item>
             </Breadcrumb>
 
             <Form>
-                {" "}
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" />
