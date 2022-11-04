8777ebedd42728cbda423c819b926335d5296863
pandad0ge <zemuskoh99@gmail.com>
2022-11-04 16:38:38 +0800

    Removed Table from Home page

--- 2102-scheduler/src/routes/Home.js
+++ 2102-scheduler/src/routes/Home.js
@@ -1,13 +1,10 @@
 import React from "react";
 import NavBar from "../components/NavBar";
-import Table from "../components/Table";
-import Button from "react-bootstrap/Button";
 
 const Home = () => {
     return (
         <div>
             <NavBar />
-            <Table />
         </div>
     );
 };
