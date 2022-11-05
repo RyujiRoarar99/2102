cc7fd1a79b675522e1ea46994e0392b01eb7029e
Zemus <85005535+pandad0ge@users.noreply.github.com>
2022-11-06 00:50:08 +0800

    Added Daily Schedule to Home Page

--- 2102-scheduler/src/routes/Home.js
+++ 2102-scheduler/src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import DailySchedule from "../components/DailySchedule";
 
 const Home = () => {
     return (
         <div>
             <NavBar />
+            <DailySchedule />
         </div>
     );
 };
