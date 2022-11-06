a20ac28226c23dc1424102351764f512116326d3
Jeriel <jerielcastro87@gmail.com>
2022-11-06 08:27:21 +0800

    Update App.js

--- 2102-scheduler/src/App.js
+++ 2102-scheduler/src/App.js
@@ -1,10 +1,7 @@
-
-
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./components/NavBar.css";
-
 
 import Home from "./routes/Home";
 import SlotAllocation from "./routes/SlotAllocation";
@@ -13,7 +10,6 @@ import EquipmentOverview from "./routes/EquipmentOverview";
 import LogRecords from "./routes/LogRecords";
 import LogInPage from './routes/LogInPage'
 import RegisterPage from './routes/RegisterPage'
-
 
 function App() {
   return (
